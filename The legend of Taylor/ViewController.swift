ad7e28b610dcf1710c2affb3f34ea2f8ee269cd9
Volm <Vom1@pcs-MacBook-Pro.local>
2016-04-08 19:33:13 -0400

    added files for TaylorsLegend

--- The legend of Taylor/ViewController.swift
+++ The legend of Taylor/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taylorLogo: UIImageView!
+    @IBOutlet weak var taylorBg: UIImageView!
+    @IBOutlet weak var adventureButton: UIButton!
+    
+    @IBOutlet weak var boomButton: UIButton!
+    @IBOutlet weak var boomLogo: UIImageView!
+    @IBOutlet weak var boomBg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startTheAdventure(sender: AnyObject) {
+        taylorLogo.hidden = false
+        taylorBg.hidden = false
+        adventureButton.hidden = true
+        boomLogo.hidden = true
+        boomBg.hidden = true
+        boomButton.hidden = true
+    }
 
+    @IBAction func blowTheAppUp(sender: AnyObject) {
+        boomLogo.hidden = false
+        boomBg.hidden = false
+        boomButton.hidden = true
+        taylorLogo.hidden = true
+        taylorBg.hidden = true
+        adventureButton.hidden = true 
+        
+    }
 }
 
